4b5a1241f3cd6acc08889357a545d315da24a5bc
hyunsik-park <hyunsik-park@aeuclid.com>
2022-01-04 16:15:41 +0900

    update environment.

--- data/environment.js
+++ data/environment.js
@@ -1,12 +1,12 @@
 
 export default class Environment {
     static get server() {
-        // return "http://ec2-13-124-80-213.ap-northeast-2.compute.amazonaws.com:8080";
-        return "http://192.168.0.148:8080";
+        return "http://ec2-3-36-74-93.ap-northeast-2.compute.amazonaws.com:8080";
+        // return "http://192.168.0.148:8080";
     }
 
     static get websocketServer() {
-        // return "ws://ec2-13-124-80-213.ap-northeast-2.compute.amazonaws.com:8080/ws";
-        return "ws://192.168.0.148:8080/ws";
+        return "ws://ec2-3-36-74-93.ap-northeast-2.compute.amazonaws.com:8080/ws";
+        // return "ws://192.168.0.148:8080/ws";
     }
 }
